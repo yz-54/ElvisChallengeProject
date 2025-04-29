7ae451a00840dec9967ca8491e660407d8af15bf
yzhang <zhangyehua1996@gmail.com>
2025-04-28 20:42:26 -0500

    init push

--- ElvisChallengeProject/ContentView.swift
+++ ElvisChallengeProject/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var img = ""
+    @State private var text = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.purple)
+
+            Spacer()
+
+            Image(systemName: img)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(text)
+                .font(.title3)
+                .fontWeight(.heavy)
+                .foregroundStyle(.purple)
+            Spacer()
+            
+            HStack {
+                Button("Peace"){
+                    img = "peacesign"
+                    text = "Peace"
+                }
+                Button("Love"){
+                    img = "suit.heart"
+                    text = "Love"
+                }
+                Button("Understanding"){
+                    img = "lightbulb"
+                    text = "Understanding"
+                }
+            }.buttonStyle(.borderedProminent)
+                .tint(.purple)
+                .font(.title2)
+                .bold()
+
         }
         .padding()
     }
