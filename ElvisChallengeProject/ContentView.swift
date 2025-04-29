06d2a70ea9a2651c2cac71c5415402329a11bf84
yzhang <zhangyehua1996@gmail.com>
2025-04-28 19:58:00 -0500

    Initial Commit

---
+++ ElvisChallengeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElvisChallengeProject
+//
+//  Created by Yehua Zhang on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElvisChallengeProject/ElvisChallengeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisChallengeProjectApp.swift
+//  ElvisChallengeProject
+//
+//  Created by Yehua Zhang on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisChallengeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
